573768be7c245410a7d54c738f2a4f148946fe67
Kuncans <akafauna@gmail.com>
2022-02-03 14:36:23 +0000

    Initial Commit

---
+++ eWallet/eWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  eWalletApp.swift
+//  eWallet
+//
+//  Created by Duncan Kent on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct eWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
