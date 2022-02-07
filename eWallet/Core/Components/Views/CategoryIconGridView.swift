f44b8344dab5e09f13d3b06a70aa4bfa7f8a7019
Kuncans <akafauna@gmail.com>
2022-02-07 14:20:17 +0000

    Update category icon grid view

--- eWallet/Core/Components/Views/CategoryIconGridView.swift
+++ eWallet/Core/Components/Views/CategoryIconGridView.swift
@@ -14,35 +14,18 @@ struct CategoryIconGridView: View {
     var body: some View {
         
         VStack {
-            
-            LazyVGrid (columns: columns) {
+            LazyVGrid (columns: columns, spacing: 22) {
                 ForEach(paymentType.allCases, id: \.self) { category in
                     CategoryIconView(color: category.paymentColor,
                                      symbol: category.paymentSymbol,
                                      text: category.rawValue.capitalized)
                 }
-                .padding()
+                .padding(.horizontal)
             }
             .padding()
-            
         }
-        
     }
 }
-//
-//Section("Card Personalization") {
-//    Picker("Card Color", selection: $newCardColor) {
-//        ForEach(cardColor.allCases, id: \.self) { color in
-//            HStack {
-//                RoundedRectangle(cornerRadius: 10)
-//                    .gradientForeground(colors: color.color)
-//                    .frame(width: 32, height: 32)
-//                    .padding(.trailing, 8)
-//                Text(color.rawValue.capitalized)
-//                    .frame(maxWidth: 60, alignment: .leading)
-//            }
-//        }
-//    }
 
 struct CategoryIconGridView_Previews: PreviewProvider {
     static var previews: some View {
--- eWallet/eWalletApp.swift
+++ eWallet/eWalletApp.swift
@@ -12,7 +12,7 @@ struct eWalletApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                HomeView()
+                CategoryIconGridView()
             }
         }
     }
